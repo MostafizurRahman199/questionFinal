432562d8a30763c1d9b564e7f9ec94e2f51fd993
MostafizurRahman199 <67595254+MostafizurRahman199@users.noreply.github.com>
2021-04-28 02:10:47 +0600

    Add files via upload

---
+++ majak.js
@@ -0,0 +1,11 @@
+
+var ok = prompt("To get all questions you have to fillup some questions write- agree/disagree")
+var name = prompt("Enter your name: ");
+var id = prompt("Enter your ID: ");
+var share = prompt("Do you share these questions to all ? YES/NO");
+
+var fatherName =prompt("Enter your father's name: ");
+var motherName = prompt("Enter your mother's name: ");
+var address = prompt("Enter your address: ");
+var age = prompt("How old are you? ");
+var interest = prompt("Your hobby: ");
